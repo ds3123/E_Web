5211d90326870d3c59996de487449cba16a20f4a
DS <ds312306@gmail.com>
2023-11-18 01:05:42 +0800

    revise next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    // distDir : 'build' ,     // 自訂 build ( 預設為 .next ) 資料夾
-    output  : "standalone"  // 啟用獨立功能。 此步驟會減少 Next.js 專案的大小，以確保它低於靜態 Web Apps 的大小限制
+    reactStrictMode : true ,
+    distDir         : 'build' ,     // 自訂 build ( 預設為 .next ) 資料夾
+    output          : "standalone"  // 啟用獨立功能。 此步驟會減少 Next.js 專案的大小，以確保它低於靜態 Web Apps 的大小限制
 
 }
 
